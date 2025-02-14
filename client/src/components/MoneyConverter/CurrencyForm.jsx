e3f6dd04fbfe19889e3189ce340c66baa35adabb
pavan1419 <ptechhat@gmail.com>
2025-02-14 14:59:45 +0530

    commit

--- client/src/components/MoneyConverter/CurrencyForm.jsx
+++ client/src/components/MoneyConverter/CurrencyForm.jsx
@@ -8,6 +8,7 @@ import {
   Button,
   CircularProgress,
   Box,
+  Paper,
 } from '@mui/material';
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import { motion } from 'framer-motion';
@@ -25,21 +26,34 @@ const CurrencyForm = ({
   onConvert,
 }) => {
   return (
-    <Box sx={{ width: '100%' }}>
+    <Paper
+      elevation={0}
+      sx={{
+        p: { xs: 2, sm: 3 },
+        bgcolor: 'background.default',
+        borderRadius: 2,
+      }}
+    >
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
         alignItems='center'
         justifyContent='center'
       >
+        {/* From Currency */}
         <Grid item xs={12} sm={5} md={4}>
           <TextField
             select
             fullWidth
-            label='From'
+            label='From Currency'
             value={fromCurrency}
             onChange={onFromCurrencyChange}
             variant='outlined'
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2,
+              },
+            }}
           >
             {availableCurrencies.map((currency) => (
               <MenuItem key={currency.code} value={currency.code}>
@@ -49,13 +63,14 @@ const CurrencyForm = ({
           </TextField>
         </Grid>
 
+        {/* Swap Button */}
         <Grid
           item
           xs={12}
           sm='auto'
           sx={{
             display: 'flex',
-            justifyContent: { xs: 'center', sm: 'flex-start' },
+            justifyContent: 'center',
             order: { xs: 3, sm: 2 },
           }}
         >
@@ -66,21 +81,35 @@ const CurrencyForm = ({
               component={motion.button}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.95 }}
-              sx={{ mx: { xs: 0, sm: 1 } }}
+              sx={{
+                mx: { xs: 0, sm: 1 },
+                bgcolor: 'background.paper',
+                boxShadow: 1,
+                '&:hover': {
+                  bgcolor: 'background.paper',
+                  boxShadow: 2,
+                },
+              }}
             >
               <SwapHorizIcon />
             </IconButton>
           </Tooltip>
         </Grid>
 
+        {/* To Currency */}
         <Grid item xs={12} sm={5} md={4} sx={{ order: { xs: 2, sm: 3 } }}>
           <TextField
             select
             fullWidth
-            label='To'
+            label='To Currency'
             value={toCurrency}
             onChange={onToCurrencyChange}
             variant='outlined'
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2,
+              },
+            }}
           >
             {availableCurrencies.map((currency) => (
               <MenuItem key={currency.code} value={currency.code}>
@@ -90,7 +119,8 @@ const CurrencyForm = ({
           </TextField>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} sx={{ order: 4 }}>
+        {/* Amount Input */}
+        <Grid item xs={12} md={6} sx={{ order: 4 }}>
           <TextField
             fullWidth
             type='number'
@@ -101,11 +131,20 @@ const CurrencyForm = ({
             helperText={amount < 0 ? "Amount can't be negative" : ''}
             variant='outlined'
             InputProps={{
-              inputProps: { min: 0 },
+              inputProps: {
+                min: 0,
+                step: '0.01',
+              },
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: 2,
+              },
             }}
           />
         </Grid>
 
+        {/* Convert Button */}
         <Grid
           item
           xs={12}
@@ -116,17 +155,22 @@ const CurrencyForm = ({
             mt: { xs: 2, sm: 3 },
           }}
         >
-          <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
+          <motion.div
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            style={{ width: '100%', maxWidth: '300px' }}
+          >
             <Button
               variant='contained'
               onClick={onConvert}
               disabled={loading || !amount || amount < 0}
+              fullWidth
               sx={{
-                px: { xs: 4, sm: 6 },
-                py: { xs: 1, sm: 1.5 },
+                py: 1.5,
                 borderRadius: 2,
                 fontSize: { xs: '1rem', sm: '1.1rem' },
-                minWidth: { xs: '200px', sm: '250px' },
+                fontWeight: 600,
+                textTransform: 'none',
                 boxShadow: 2,
                 '&:hover': {
                   boxShadow: 4,
@@ -136,14 +180,17 @@ const CurrencyForm = ({
               {loading ? (
                 <CircularProgress size={24} color='inherit' />
               ) : (
-                'Convert'
+                'Convert Currency'
               )}
             </Button>
           </motion.div>
         </Grid>
       </Grid>
-    </Box>
+    </Paper>
   );
 };
 
 export default CurrencyForm;
+
+
+
