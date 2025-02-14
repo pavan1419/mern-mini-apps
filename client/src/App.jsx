bba19d99905ae291b9d13ba0192a25dccc0ae368
pavan1419 <ptechhat@gmail.com>
2025-02-14 14:13:44 +0530

    3 mini apps and backend

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box, Container, Paper } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Counter from './components/Counter';
+import { Clock, ClockProvider } from './components/Clock';
+import './App.css';
+
+const App = ({ toggleTheme }) => {
+  return (
+    <Router>
+      <ClockProvider>
+        <Paper
+          sx={{
+            minHeight: '100vh',
+            borderRadius: 0,
+            backgroundColor: 'background.default',
+          }}
+        >
+          <Navbar toggleTheme={toggleTheme} />
+          <Container maxWidth='lg'>
+            <Box sx={{ py: 3 }}>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/counter' element={<Counter />} />
+                <Route path='/clock' element={<Clock />} />
+              </Routes>
+            </Box>
+          </Container>
+        </Paper>
+      </ClockProvider>
+    </Router>
+  );
+};
+
+export default App;
